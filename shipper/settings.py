ed266a0efbb01560810102eba7f5870f0e75c34b
Kharay33455 <shakibkhan276337@gmail.com>
2025-01-02 20:11:54 +0100

    Lats

--- shipper/settings.py
+++ shipper/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['www.dosojin.online', 'dosojin.online', 'vercel-ouez.onrender.com']
+ALLOWED_HOSTS = ['vercel-ouez.onrender.com']
 
 
 # Application definition
