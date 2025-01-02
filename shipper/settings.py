9188277a8fe0207462bd1412c03b289309d72a6f
Kharay33455 <shakibkhan276337@gmail.com>
2025-01-02 20:37:55 +0100

    Last

--- shipper/settings.py
+++ shipper/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['dosojincargos.online','vercel-ouez.onrender.com']
 
@@ -134,7 +134,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS =[BASE_DIR/'static']
 STATIC_ROOT = os.path.join(BASE_DIR, 'static-files')
 
 MEDIA_URL = ''
