27cdad8bd53a0cb95cb32eb77af8fbe4afd95374
Kharay33455 <shakibkhan276337@gmail.com>
2025-02-05 19:31:23 +0100

    new DB

--- shipper/settings.py
+++ shipper/settings.py
@@ -32,8 +32,8 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['dosojincargos.online','vercel-ouez.onrender.com']
-
+#ALLOWED_HOSTS = ['dosojincargos.online','vercel-ouez.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -98,12 +98,14 @@ WSGI_APPLICATION = 'shipper.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-#DATABASES = {
- #   'default': {
-  #      'ENGINE': 'django.db.backends.sqlite3',
-   #     'NAME': BASE_DIR / 'db.sqlite3',
- #   }
-#}
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+"""
 
 
 
